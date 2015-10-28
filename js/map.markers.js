7db4e6dd9976dea70e81470212bc441b47afaa37
AlexBrown-appnovation <alex@appnovation.com>
2015-10-27 18:31:38 -0700

    initial commit

---
+++ js/map.markers.js
@@ -0,0 +1,45 @@
+//List of map markers
+var map_markers = [
+	{
+		"title": "Reception",
+		"latitude":49.548082,
+		"longitude":-119.568996,
+		"icon":"fa-glass", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Bench 1775 Winery <br> 1775 Naramata Rd, Penticton, BC V2A 8T8"
+	},
+	{
+		"title": "Accommodation 1",
+		"latitude":33.777929,
+		"longitude":-118.076891,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Accommodation 1 <br> Rossmoor, CA 90720"
+	},
+	{
+		"title": "Accommodation 2",
+		"latitude":33.780428,
+		"longitude":-118.084075,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Accommodation 2 <br> Los Alamitos, CA 90720"
+	},
+	{
+		"title": "Accommodation 3",
+		"latitude":33.777551,
+		"longitude":-118.050468,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Accommodation 3 <br> Seal Beach, CA 90740"
+	},
+	{
+		"title": "Transportation",
+		"latitude":49.463258,
+		"longitude":-119.602233,
+		"icon":"fa-plane", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Seal Beach VORTAC SLI <br> Los Alamitos, CA 90720"
+	},
+	{
+		"title": "Ceremony",
+		"latitude":49.547922,
+		"longitude":-119.571107,
+		"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Bench 1775 Winery <br> 1775 Naramata Rd, Penticton, BC V2A 8T8"
+	}
+];
